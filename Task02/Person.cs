b4be9908ebda012dcaf48fe3985190a3d03ab524
Artem <a.khimin@yandex.ru>
2021-05-19 18:32:57 +0300

    Подготовка перед  ildasm/ilasm

---
+++ Task02/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+
+
+        public void GetFullName()
+        {
+            Console.WriteLine($" Имя: {FirstName}, Фамилия: {LastName}, Отчество: {Patronymic}");
+
+        }
+
+    }
+
+}
