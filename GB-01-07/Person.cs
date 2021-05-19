643d93aca727678a6f4ab5c45f9ba498cc3365fd
Artem <a.khimin@yandex.ru>
2021-05-19 12:32:17 +0300

    Добавьте файлы проекта.

---
+++ GB-01-07/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_01_07
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+
+
+        public void GetFullName()
+        {
+            Console.WriteLine($" Имя: {FirstName}, Фамилия: {LastName}, Отчество: {Patronymic}");
+
+        }
+
+    }
+}
---
+++ GB-01-07/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GB_01_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person() { FirstName = "Уильям ", LastName = "Генри ", Patronymic = "Гейтс III" });
+            people.Add(new Person() { FirstName = "Марк  ", LastName = "Эллиот  ", Patronymic = "Цукерберг " });
+            people.Add(new Person() { FirstName = "Илон  ", LastName = "Рив  ", Patronymic = "Маск " });
+
+
+            Person person = new Person();
+
+            Console.WriteLine("Введите Имя: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Введите Фамилию: ");
+            person.LastName = Console.ReadLine();
+
+            Console.WriteLine("Введите Отчество: ");
+            person.Patronymic = Console.ReadLine();
+            Console.WriteLine(new string('*',30));
+
+            foreach (var p in people)
+            {
+                GetFullName(p.FirstName, p.LastName, p.Patronymic);
+            }
+
+            person.GetFullName();
+
+        }
+
+        public static void GetFullName(string firstName, string lastName, string patronymic)
+        {
+            Console.WriteLine($" Имя: {firstName}, Фамилия: {lastName}, Отчество: {patronymic}");
+
+        }
+    }
+}
